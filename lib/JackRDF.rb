59e655b6ff6988d43e537bade7fc7e5afbcc24ad
caesarfeta <adamtavares@gmail.com>
2014-10-17 15:45:30 -0400

    Implementing CITE_URN support.

--- lib/JackRDF.rb
+++ lib/JackRDF.rb
@@ -8,20 +8,30 @@ class JackRDF
   def initialize( endp )
     @endp = endp
     @sparql = SparqlQuick.new( @endp )
+    @urn_verb = "http://github.com/caesarfeta/JackSON/docs/SCHEMA.md#urn"
+    @src_verb = "http://github.com/caesarfeta/JackSON/docs/SCHEMA.md#src"
   end
   
-  # urn { String } Subject URN
+  # url { String } URL to JSON
   # file { String } Path to file
-  def post( urn, file )
+  def post( url, file )
+    urn = url
+    
     # Does this already exist?
-    if @sparql.count([ urn.tagify,:p,:o ]) > 0
+    if @sparql.count([ url.tagify,:p,:o ]) > 0
       throw "#{urn} graph already exists. Use .put()"
     end
     
+    # Turn JSON into a hash for checking
     hash = to_hash( File.read( file ) )
     if hash.has_key?('@context') == false
       throw "#{file} is not JSON-LD"
     end
+    context = hash['@context']
+    
+    # Add src
+    context['src'] = @src_verb
+    hash['src'] = url
     
     # The urn to the JSON file becomes 
     # the JSON-LD id which becomes the
@@ -31,20 +41,32 @@ class JackRDF
     # Convert to JSON-LD then to RDF
     jsonld = to_jsonld( hash )
     rdf = to_rdf( jsonld )
-        
+    
+    # CITE URN support
+    if hash.has_key?('urn') == true 
+      if context.has_key?('urn') && context['urn'] == @urn_verb
+        urn_rdf = RDF::Graph.new
+        rdf.each do |tri|
+          tri.subject = RDF::Resource.new( hash['urn'] )
+          urn_rdf << tri
+        end
+        rdf = urn_rdf
+      end 
+    end
+    
     # Insert the RDF data
     @sparql._update.insert_data( rdf )
   end
   
-  # urn { String } Subject URN
+  # url { String } URL to JSON file
   # file { String } Path to file
-  def put( urn, file )
+  def put( url, file )
     delete( urn )
     post( urn, file )
   end
   
-  # urn { String } Subject URN
-  def delete( urn )
+  # url { String } URL to JSON file
+  def delete( url, file )
     @sparql.delete([ urn.tagify,:p,:o ])
   end
   

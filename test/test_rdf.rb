9bc84e2df8fe880c5059fb493478a75dd3067a56
caesarfeta <adamtavares@gmail.com>
2014-11-04 17:09:13 -0500

    Still need to finish turning test_notes.md into proper tests, but I think I'm ready to merge.

--- test/test_rdf.rb
+++ test/test_rdf.rb
@@ -12,10 +12,10 @@ class TestRdf < Minitest::Test
   
   # Make sure post method uses URL if no @id value is present
   def test_AAA_post
-    Fuseki.empty
-    rdf = Fuseki.handle
+    Help.empty
+    rdf = Help.handle
     rdf.post( 'http://localhost:4567/test/urn/1', 'sample/post.json' )
-    check = Fuseki.get
+    check = Help.get
     check.each do |tri|
       if tri["s"]["value"] != 'http://localhost:4567/test/urn/1'
         assert( false )
@@ -27,18 +27,18 @@ class TestRdf < Minitest::Test
   
   # Make sure triples are deleted entirely
   def test_AAB_delete
-    Fuseki.empty
-    rdf = Fuseki.handle
+    Help.empty
+    rdf = Help.handle
     rdf.post( 'http://localhost:4567/test/urn/1', 'sample/post.json' )
     rdf.delete( 'http://localhost:4567/test/urn/1', 'sample/post.json' )
-    check = Fuseki.get
+    check = Help.get
     assert_equal( check.length, 0 )
   end
   
   # Make sure double posts are blocked
   def test_AAC_double_post_block
-    Fuseki.empty
-    rdf = Fuseki.handle
+    Help.empty
+    rdf = Help.handle
     rdf.post( 'http://localhost:4567/test/urn/1', 'sample/post.json' )
     begin
       rdf.post( 'http://localhost:4567/test/urn/1', 'sample/post.json' )
@@ -52,10 +52,10 @@ class TestRdf < Minitest::Test
   # Make sure CITE URNs are used as subject nodes 
   # when id_mode conditions are met
   def test_AAD_cite_urn_subject
-    Fuseki.empty
-    rdf = Fuseki.handle
+    Help.empty
+    rdf = Help.handle
     rdf.post( 'http://localhost:4567/test/urn/1', 'sample/id.json' )
-    url = Fuseki.get
+    url = Help.get
     file = rdf.file_to_hash( 'sample/id.json' )
     url.each do |tri|
       if tri["s"]["value"] != file['@id']
@@ -69,24 +69,43 @@ class TestRdf < Minitest::Test
   # Make sure all sources are recorded 
   # when using CITE URNs as subject nodes
   def test_AAE_cite_urn_multi
-    Fuseki.empty
-    rdf = Fuseki.handle
+    Help.empty
+    rdf = Help.handle
     (1..3).each do |n|
       rdf.post( "http://localhost:4567/test/urn/#{n}", "sample/cite/urn_0#{n}.json" )
     end
-    check = Fuseki.get
+    check = Help.get
     count = 0
     check.each do |tri|
-      if tri["p"]["value"] == "http://github.com/caesarfeta/JackRDF/blob/master/docs/SCHEMA.md#src"
+      if tri["p"]["value"] == Help.src
         count += 1
       end
     end
     assert_equal( count, 3 )
   end
   
+  # Make sure sources are being deleted properly
+  # when using CITE URNs with multiple JSON sources
+  def test_AAF_cite_urn_multi_delete
+    Help.empty
+    rdf = Help.handle
+    (1..3).each do |n|
+      rdf.post( "http://localhost:4567/test/urn/#{n}", "sample/cite/urn_0#{n}.json" )
+    end
+    (1..2).each do |n|
+      rdf.delete( "http://localhost:4567/test/urn/#{n}", "sample/cite/urn_0#{n}.json" )
+    end
+    check = Help.get
+    puts check.inspect
+    assert( true )
+  end
 end
 
-class Fuseki
+class Help
+  def self.src
+    "http://github.com/caesarfeta/JackRDF/blob/master/docs/SCHEMA.md#src"
+  end
+  
   def self.handle
     JackRDF.new( self.ds )
   end
